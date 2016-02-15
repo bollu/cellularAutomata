6c7c10e3197584bb379608ad36c54cc4cb4a9a58
bollu <siddu.druid@gmail.com>
2016-02-15 12:01:15 +0530

    fixed missing \ in file

--- build-and-profile.sh
+++ build-and-profile.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
-stack build --executable-profiling --library-profiling --ghc-options="-fprof-auto -rtsopts" \
+
+stack build --executable-profiling --library-profiling --ghc-options="-fprof-auto -rtsopts" && \
 echo "profiling ----" && \
 stack exec haskell-diagrams-cellular-automata-exe  --   -h 400 -w 400 -o images/simple.gif +RTS -p
