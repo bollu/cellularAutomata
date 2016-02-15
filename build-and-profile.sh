c7eda37c3d79dd057cac13150eb550929ca94a2c
bollu <siddu.druid@gmail.com>
2016-02-15 12:00:12 +0530

    add profiling options to stack build

--- build-and-profile.sh
+++ build-and-profile.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-stack build && \
+stack build --executable-profiling --library-profiling --ghc-options="-fprof-auto -rtsopts" \
 echo "profiling ----" && \
 stack exec haskell-diagrams-cellular-automata-exe  --   -h 400 -w 400 -o images/simple.gif +RTS -p
